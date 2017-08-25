9de4737df211783e99d5009d55eea0ba3bb2c93c
Joshua Mabina <joshua@irabu.co.tz>
2017-08-25 11:25:10 +0300

    Add translation assets for the dashboard
    new file:   resources/lang/en/dashboard.php

---
+++ resources/lang/en/dashboard.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Dashboard Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used on the administrator home page for various
+    | keywords that we need to display to the user.
+    |
+    */
+
+    'title' => 'Your Dashboard',
+    'description' => 'Provides an overview of your website and access to other sections.',
+
+];
